a6671efb649ddd3ba34ab19d002dc13b93083997
Joshua Oladele <jurshsmith@gmail.com>
2023-09-15 15:49:41 +0100

    Fix TestDB setup

--- chaindexing-tests/src/main.rs
+++ chaindexing-tests/src/main.rs
@@ -1,7 +1,15 @@
+use chaindexing::{ChaindexingRepo, Migratable, Repo};
 use chaindexing_tests::db;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Run once to setup database
     // Useful in a CI environment running parallel tests
     db::setup();
+
+    let repo = ChaindexingRepo::new(db::database_url().as_str());
+    let pool = repo.get_pool(1).await;
+    let mut conn = ChaindexingRepo::get_conn(&pool).await;
+
+    repo.migrate(&mut conn).await;
 }
