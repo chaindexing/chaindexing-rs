af196a22ecd3dbe042409674929dc9bd3d346697
Joshua Oladele <jurshsmith@gmail.com>
2024-04-13 14:07:06 +0100

    Encapsulate OptimizationConfig

--- chaindexing/src/config.rs
+++ chaindexing/src/config.rs
@@ -28,12 +28,24 @@ impl std::fmt::Debug for ConfigError {
 
 #[derive(Clone, Debug)]
 pub struct OptimizationConfig {
-    pub keep_node_active_request: KeepNodeActiveRequest,
+    pub(crate) keep_node_active_request: KeepNodeActiveRequest,
     /// Optimization starts after the seconds specified here.
     /// This is the typically the estimated time to complete initial indexing
     /// i.e. the estimated time in seconds for chaindexing to reach
     /// the current block for all chains being indexed.
-    pub optimize_after_in_secs: u64,
+    pub(crate) optimize_after_in_secs: u64,
+}
+
+impl OptimizationConfig {
+    pub fn new(
+        keep_node_active_request: &KeepNodeActiveRequest,
+        optimize_after_in_secs: u64,
+    ) -> Self {
+        Self {
+            keep_node_active_request: keep_node_active_request.clone(),
+            optimize_after_in_secs,
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
